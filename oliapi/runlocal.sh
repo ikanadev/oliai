726765c5ffe45e09725245c88e20f015852e5bd6
Kevin Vargas <vargaskevv@gmail.com>
2024-03-07 23:57:22 -0400

    Add open ai key to runlocal

--- oliapi/runlocal.sh
+++ oliapi/runlocal.sh
@@ -3,5 +3,6 @@ export JWT_SIGN_KEY=secret
 export DATABASE=postgresql://kevv:postgres@0.0.0.0:5432/olidb?sslmode=disable
 export MIGRATIONS_URL=github://ikanadev/oliai/oliapi/db/migrations
 export QDRANT_URL=127.0.0.1:6334
+export OPEN_AI_KEY=myapikey
 
 go run cmd/api/api.go
