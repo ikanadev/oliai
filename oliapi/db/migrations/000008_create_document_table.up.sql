f516564f7732609c2abda0a1f9d9127d2daf418b
Kevin Vargas <vargaskevv@gmail.com>
2024-03-09 03:28:56 -0400

    add embedding date field to documents

--- oliapi/db/migrations/000008_create_document_table.up.sql
+++ oliapi/db/migrations/000008_create_document_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE documents (
 	content TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
+	embedding_date TIMESTAMP DEFAULT NULL,
 	deleted_at TIMESTAMP DEFAULT NULL,
 	archived_at TIMESTAMP DEFAULT NULL,
 	FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
